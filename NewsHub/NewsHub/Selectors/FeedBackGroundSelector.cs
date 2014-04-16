f600d6adcc5c040b6dd53a557a2545d6bd904a5f
xedeen <xedin.el.fuego@gmail.com>
2014-04-16 19:18:50 +0300

    NewsHub

---
+++ NewsHub/NewsHub/Selectors/FeedBackGroundSelector.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace NewsHub.Selectors
+{
+    public sealed class FeedBackGroundSelector : IValueConverter
+    {
+        public FeedBackGroundSelector()
+        {}
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var isClearTemplate = (bool)value;
+            if (!isClearTemplate && false)
+            {
+                var lightThemeEnabled = (Visibility)Application.Current.Resources
+                        ["PhoneLightThemeVisibility"] == Visibility.Visible;
+
+                var url = lightThemeEnabled
+                    ? "/Onliner;component/Assets/ocean_lights_light.png"
+                    : "/Onliner;component/Assets/leather.png";
+                var brush = new ImageBrush { ImageSource = new BitmapImage(new Uri(url, UriKind.Relative)) };
+                return brush;
+            }
+            //return (Brush) Application.Current.Resources["PhoneBackgroundBrush"];
+            //return (Brush) Application.Current.Resources["PhoneForegroundBrush"];
+            return (Brush)new SolidColorBrush(Color.FromArgb(255, 250, 247, 238));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
